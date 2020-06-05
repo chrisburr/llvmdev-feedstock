bc8b962a6f5ce3023bb4b00711e91babba086d1f
Chris Burr <christopher.burr@cern.ch>
2020-06-05 16:42:23 +0200

    Enable a debug build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@ mkdir build
 cd build
 
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_BUILD_TYPE=Debug \
       -DLLVM_ENABLE_RTTI=ON \
       -DLLVM_INCLUDE_TESTS=OFF \
       -DLLVM_INCLUDE_UTILS=OFF \
